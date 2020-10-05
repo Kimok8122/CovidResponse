d096171a7857c7d54a8673376774043a2f0e08c0
KimoK8122 <kimo.k@me.com>
2020-10-05 08:37:29 -0600

    added Dapper and MySQL.data

---
+++ CovidReponse/Models/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CovidReponse.Models
+{
+    public class user
+    {
+        public user()
+        {
+        }
+
+        public int user_ID { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public DateTime creae_date { get; set; }
+    }
+}
